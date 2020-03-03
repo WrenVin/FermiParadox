64cb8ff1045282ae2a77822dd104e13b7784a38e
Vincent Wren <wrenvin@gmail.com>
2020-03-03 10:05:53 -0600

    Drake Comments

---
+++ main.py
@@ -0,0 +1,10 @@
+# Drake Equation: N = R * Fp * ne * F1 * Fi * Fc * L
+# N is the number of civilizations in the galaxy that have detectable electromagnetic emmisions
+# R is the Average rate of star formation in the galaxy (New stars per year)
+# Fp is the fraction of start with planets I.E.: 1/8
+# ne is the average number of plantes with an enviorment suited for life for planets with stars
+# F1 is the fraction of planets that develop life
+# Fi is the fraction of habitable planets with inteligent life
+# Fc is the fraction of civilizations that release decectable signs of their existence into space
+# L is the length of time in light years over which civilizations release detectable signals
+
